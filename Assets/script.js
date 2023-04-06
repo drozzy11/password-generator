487214496a473c2b38a5954f9d20979d90896cf3
drozzy11 <ruiz.diego071299@gmail.com>
2023-04-06 01:42:31 -0500

    Fixed issue with password not displaying! however still displaying undefined

--- Assets/script.js
+++ Assets/script.js
@@ -11,6 +11,7 @@ function criteria() {
 var valid = false
 do {
 var length = prompt("Choose a password between 8 and 128 characters.");
+var chooseNumbers = confirm('Do you want your password to contain numbers?');
 var chooseupper = confirm("Do you want your password to contain upper case letters?");
 var chooselower = confirm("Do you want your password to contain lower case letters?");
 var choosesymbols = confirm("Do you want your password to contain symbols?");
@@ -19,7 +20,7 @@ var responses = length, chooseupper, chooselower, choosesymbols;
 
 if ((length < 8) || (length > 128))
   alert("Please choose a password between 8 and 128");
-else if ((!chooseupper) && (!chooselower) && (!choosesymbols))
+else if ((!chooseupper) && (!chooselower) && (!choosesymbols) && (!chooseNumbers))
   alert ("You must choose atleast one!");
 else 
  valid = true ;
@@ -34,8 +35,34 @@ function generatePassword() {
   var combo = [];
   var randomPassword =[""];
 
-if (passwordChoices.length)
-  push()
+  if (passwordChoices.chooseNumbers) {
+    for (var i of numbers)
+      combo.push(i);
+  }
+  if (passwordChoices.chooselower) {
+    for (var i of smallLetters)
+      combo.push(i);
+  }
+  if (passwordChoices.chooseupper) {
+    for (var i of bigLetters)
+      possibleCombo.push(i);
+  }
+  if (passwordChoices.choosesymbols) {
+    for (var i of symbols)
+      possibleCombo.push(i);
+  }
+
+
+  console.log(combo);
+
+
+  for (var i = 0; i < passwordChoices.length; i++) {
+   randomPassword += combo[Math.floor(Math.random() * combo.length)];
+    
+  }
+  console.log(randomPassword);
+
+  return(randomPassword);
 }
 
 // Write password to the #password input
